644dfd06cb4ee32d97998e00ede4884b8907b794
mohamed mahrous <adrakola67@gmail.com>
2024-09-29 13:29:52 +0300

    extension for handle singleOutput

---
+++ Extensions/String+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  StringEx.swift
+//  IOSTask
+//
+//  Created by mohamed mahrous on 26/09/2024.
+//
+
+import UIKit
+
+extension String {
+    var int: Int? {
+        return Int(self)
+    }
+}
